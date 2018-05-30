e2e4a2188e3329aed3a396b039eed5872debd493
xiawang1024 <xiawang1024@icloud.com>
2018-05-30 22:25:39 +0800

    feat:socket.io练习

--- router/socket.js
+++ router/socket.js
@@ -4,9 +4,16 @@ module.exports = {
 	initSocket(http) {
 		const io = socketIo(http);
 		io.on('connection', (socket) => {
-			socket.broadcast.emit('user connected', 'user connected');
-			socket.emit('yourself msg', '私人消息');
-			io.emit('all msg', '公开消息');
+			console.log('a user connected');
+			socket.on('disconnect', () => {
+				console.log('user disconnected');
+			});
+			socket.emit('hi');
+			socket.broadcast.emit('a user coming');
+			socket.on('chat message', function(msg) {
+				console.log(msg);
+				io.emit('chat message', msg);
+			});
 		});
 	}
 };
