70c77562e3612b62f311bfbfad806e278c2fb0f7
xiawang1024 <xiawang1024@icloud.com>
2018-10-29 21:42:26 +0800

    wechat

--- controlers/wechat.js
+++ controlers/wechat.js
@@ -1,36 +1,25 @@
-const axios = require('axios')
-const appID = 'wx177b80ba98f18011'
-const secret = 'f8c4c2d3036e1eb4a269c3be4a10a8a6'
+const axios = require('axios');
+const appID = 'wx177b80ba98f18011';
+const secret = 'f8c4c2d3036e1eb4a269c3be4a10a8a6';
 module.exports = {
-  async getCode(ctx) {
-    let redirectUrl = 'http://127.0.0.1:3002/wechat/getUserInfo'
-    let authorizeUrl =
-      `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appID}&redirect_uri=` +
-      `${redirectUrl}&response_type=code&scope=snsapi_userinfo&state=wx1024#wechat_redirect`
-    ctx.redirect(authorizeUrl)
-  },
-  async getUserInfo(ctx) {
-    let ctx_query = ctx.query
-    let code = ctx_query.code
-
-    let userInfo = await getAccessToken(code)
-    console.log('------------------------------------')
-    console.log(userInfo)
-    console.log('------------------------------------')
-    ctx.render('weChat/index', { userInfo: userInfo })
-  }
-}
-
-function getAccessToken(code) {
-  let getAccessTokenUrl = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appID}&secret=${secret}&code=${code}&grant_type=authorization_code`
-  axios.get(getAccessTokenUrl).then(res => {
-    let { access_token, openid } = res.data
-
-    let getUserInfoUrl = `https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`
-    axios.get(getUserInfoUrl).then(async res => {
-      return new Promise((resolve, reject) => {
-        resolve(res.data)
-      })
-    })
-  })
-}
+	async getCode(ctx) {
+		let redirectUrl = 'http://127.0.0.1:3002/wechat/getUserInfo';
+		let authorizeUrl =
+			`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appID}&redirect_uri=` +
+			`${redirectUrl}&response_type=code&scope=snsapi_userinfo&state=wx1024#wechat_redirect`;
+		ctx.redirect(authorizeUrl);
+	},
+	async getUserInfo(ctx) {
+		try {
+			let { code } = ctx.query;
+			let getAccessTokenUrl = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appID}&secret=${secret}&code=${code}&grant_type=authorization_code`;
+			let accessTokenRes = await axios.get(getAccessTokenUrl);
+			let { access_token, openid } = accessTokenRes.data;
+			let getUserInfoUrl = `https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`;
+			let userInfoRes = await axios.get(getUserInfoUrl);
+			await ctx.render('weChat/index', { userInfo: userInfoRes.data });
+		} catch (error) {
+			console.log(error);
+		}
+	}
+};
